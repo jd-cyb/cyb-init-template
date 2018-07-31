0dcc344591b8bb9a066a57ce957143b292b12085
furic <furic@qq.com>
2018-07-31 17:07:39 +0800

    v1.0.1

--- config.js
+++ config.js
@@ -107,9 +107,9 @@ module.exports = {
   filters: {
     '.eslintrc.js': 'lint',
     '.eslintignore': 'lint',
-    'src/views/demo-jquery/': "userModel && modelConfig.includes('jquery')",
-    'src/views/demo-react/': "userModel && modelConfig.includes('react')",
-    'src/views/demo-vue/': "userModel && modelConfig.includes('vue')",
-    'webpack.config.vue.js': "userModel && modelConfig.includes('vue')"
+    'src/views/demo-jquery/': "userModel && modelConfig === 'jquery'",
+    'src/views/demo-react/': "userModel && modelConfig === 'react'",
+    'src/views/demo-vue/': "userModel && modelConfig === 'vue'",
+    'webpack.config.vue.js': "userModel && modelConfig === 'vue'"
   }
 }
