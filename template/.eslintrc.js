eb487615828dc662838c4df01adc0ffd364da43d
furic <furic@qq.com>
2018-08-16 11:52:37 +0800

    react eslint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -12,8 +12,24 @@
  */
 
 module.exports = {
-    root: true,
+    root: true,{{#if_eq modelConfig "react"}}
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+            // default to "createReactClass"
+            "pragma": "React", // Pragma to use, default to "React"
+            "version": "16.4", // React version, default to the latest React stable release
+            "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": ["forbidExtraProps"] // The names of any functions used to wrap the
+        // propTypes object, e.g. `forbidExtraProps`.
+        // If this isn't set, any propTypes wrapped in
+        // a function will be skipped.
+    },{{/if_eq}}
     parserOptions: {
+        {{#if_eq modelConfig "react"}}"ecmaFeatures": {
+            "jsx": true
+        },{{/if_eq}}
         ecmaVersion: 6,
         sourceType: 'module',
         parser: 'babel-eslint'
@@ -29,14 +45,22 @@ module.exports = {
         es6: true{{#if_eq modelConfig "jquery"}},
         jquery: true{{/if_eq}}
     },{{#if_eq lintConfig "standard"}}
-    extends: [{{#if_eq modelConfig "vue"}}'plugin:vue/essential',{{/if_eq}}{{#if_eq modelConfig "react"}}"eslint:recommended","plugin:react/recommended",{{/if_eq}}
+    extends: [
+        {{#if_eq modelConfig "vue"}}'plugin:vue/essential',{{/if_eq}}
+        {{#if_eq modelConfig "react"}}"eslint:recommended",
+        "plugin:react/recommended",{{/if_eq}}
         // https://github.com/standard/standard/blob/master/docs/RULES-en.md
         'standard'
     ],{{/if_eq}}{{#if_eq lintConfig "airbnb"}}
-    extends: [{{#if_eq modelConfig "react"}}"eslint:recommended","plugin:react/recommended",{{/if_eq}}{{#if_eq modelConfig "vue"}}'plugin:vue/essential',{{/if_eq}}
+    extends: [
+        {{#if_eq modelConfig "react"}}"eslint:recommended",
+        "plugin:react/recommended",{{/if_eq}}
+        {{#if_eq modelConfig "vue"}}'plugin:vue/essential',{{/if_eq}}
         'airbnb-base'
     ],{{/if_eq}}{{#if_eq lintConfig "none"}}
-    extends: [{{#if_eq modelConfig "vue"}}'plugin:vue/essential'{{/if_eq}}{{#if_eq modelConfig "react"}}"eslint:recommended","plugin:react/recommended"{{/if_eq}}
+    extends: [
+      {{#if_eq modelConfig "vue"}}'plugin:vue/essential'{{/if_eq}}{{#if_eq modelConfig "react"}}"eslint:recommended",
+      "plugin:react/recommended"{{/if_eq}}
     ],{{/if_eq}}
     plugins: [{{#if_eq modelConfig "vue"}}'vue'{{/if_eq}}{{#if_eq modelConfig "react"}}'react'{{/if_eq}}],
     rules: {
