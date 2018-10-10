289298bee92cdd2a22d2b7e6e5fe0db6527f9fa3
furic <furic@qq.com>
2018-10-10 15:51:51 +0800

    增加babel-loader编译

---
+++ template/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options:{
+          babelrc:true
+        }
+      }
+    }]
+  }
+}
